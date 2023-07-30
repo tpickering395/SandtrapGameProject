2a80b75a3f8bd1408ee0c6c0069d306e94b162fc
Larell Williams <superawesome289@gmail.com>
2023-07-30 13:28:01 -0700

    Added item class

---
+++ Assets/Prefabs/Items/Scripts/Item.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(filename = "New Item", menuName = "Item/Create")]
+
+public class Item : ScriptableObject
+{
+    public int id;
+    public string itemName;
+    public int value;
+    public Sprite icon;
+}
